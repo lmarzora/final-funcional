9a0a029053fb7e1b701373601540dcbc5a2b6bca
lmarzora <lmarzora@itba.edu.ar>
2017-12-04 01:41:29 -0300

    changed resolver

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
 module Main where
 
-import Lib
-
+import System.Environment (getArgs)
+import Control.Distributed.Process
+import Control.Distributed.Process.Node (initRemoteTable, runProcess)
+import Control.Distributed.Process.Backend.SimpleLocalnet
+import Control.Monad (forever, forM_)
+    
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+
+  case args of
+    ["master", host, port] -> do
+      backend <- initializeBackend host port initRemoteTable
+      startMaster backend (master backend)
+    ["slave", host, port] -> do
+      backend <- initializeBackend host port initRemoteTable
+      startSlave backend
+
+master :: Backend -> [NodeId] -> Process ()
+master backend slaves = do
+  -- Do something interesting with the slaves
+  liftIO . putStrLn $ "Slaves: " ++ show slaves
+  -- Terminate the slaves when the master terminates (this is optional)
+  terminateAllSlaves backend
